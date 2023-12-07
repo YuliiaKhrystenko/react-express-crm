bada5b0ff9472b3d7ab20a7b2716d01d6a8502da
Yuliia <yu6785963@gmail.com>
2023-12-07 10:20:19 +0200

    Create Login/Register forms

--- client/src/pages/register/index.tsx
+++ client/src/pages/register/index.tsx
@@ -1,10 +1,34 @@
 import React from 'react'
+import { Layout } from '../../components/layout'
+import { Card, Form, Row, Space, Typography } from 'antd'
+import { CustomInput } from '../../components/custom-input'
+import { PasswordInput } from '../../components/password-input/input'
+import { CustomButton } from '../../components/custom-button'
+import { Link } from 'react-router-dom'
+import { Paths } from '../../paths'
 
 const Register = () => {
   return (
-    <div>
-      register
-    </div>
+    <Layout>
+      <Row align="middle" justify="center">
+        <Card title="Register" style={{ width: "30rem" }}>
+          <Form onFinish={() => null}>
+            <CustomInput name='name' placeholder='Name' />
+            <CustomInput type='email' name='email' placeholder='Email' />
+            <PasswordInput name='password' placeholder='Password' />
+            <PasswordInput name='confirmPassword' placeholder='Confirm Password' />
+            <CustomButton type='primary' htmlType='submit'>
+              Create account
+            </CustomButton>
+          </Form>
+          <Space direction='vertical' size='large'>
+            <Typography.Text>
+              Already have an account? <Link to={Paths.login}>Log In</Link>
+            </Typography.Text>
+          </Space>
+        </Card>
+      </Row>
+    </Layout>
   )
 }
 
